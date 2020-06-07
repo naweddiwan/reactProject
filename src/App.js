93a7b0d6b5e5835be33ca9de08c9daf069226de4
NAWED <nawedn@gmail.com>
2020-06-07 18:17:25 +0530

    changed the background color and item color

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import FetchingData from './components/FetchingData';
 function App() {
   return (
     <div className="App">
-      {/* <PostList /> */}
         <h1>Data List</h1>
       
       <FetchingData/>
